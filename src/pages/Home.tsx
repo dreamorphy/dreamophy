d34fcbc3ebf60173d0b6c412e0d0f3ee9b0a138d
장현웅 <jhw81111@gmail.com>
2020-12-08 17:16:27 +0900

    홈화면진행중

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,10 +7,17 @@ const Home: React.FC = () => {
   return (
     <Container>
       <HomeContainer>
-        <BackgroundImage src={'assets/images/interior.png'} />
+        <BackgroundImage src={'assets/images/background-base.png'} />
         <ClockImg src={'assets/images/emotion receptor.gif'} onClick={() => isModalOpen(true)} />
         <MorphoImg src={'assets/images/morpho.gif'} />
-        <FontImg src={'assets/images/showcase typo.gif'} />
+        <DonutFontImg src={'assets/images/showcase typo.gif'} />
+        <DreamorphyFontImg src={'assets/images/logo.gif'} />
+        <RabbitDonutImg src={'assets/images/rabbit.png'} />
+        <FoxDonutImg src={'assets/images/fox.png'} />
+        <CatDonutImg src={'assets/images/cat.png'} />
+        <BirdDonutImg src={'assets/images/bird.png'} />
+        <HedgehogDonutImg src={'assets/images/hedgehog.png'} />
+        <FishDonutImg src={'assets/images/fish.png'} />
       </HomeContainer>
       <Modal
         isOpen={modalOpen}
@@ -41,7 +48,7 @@ const LinkImg = styled.img`
 
 const ClockImg = styled(LinkImg)`
   position: absolute;
-  width: 25%;
+  width: 26%;
   top: 31.5%;
   left: 3%;
 `;
@@ -50,13 +57,56 @@ const MorphoImg = styled(LinkImg)`
   position: absolute;
   width: 16.5%;
   top: 35.5%;
-  left: 29%;
+  left: 29.5%;
 `;
-const FontImg = styled(LinkImg)`
+const DonutFontImg = styled(LinkImg)`
   position: absolute;
   width: 23.5%;
-  top: 32.5%;
+  top: 30.5%;
+  left: 63.5%;
+`;
+const DreamorphyFontImg = styled(LinkImg)`
+  position: absolute;
+  width: 28.5%;
+  top: 19%;
+  left: 36%;
+`;
+const RabbitDonutImg = styled(LinkImg)`
+  position: absolute;
+  width: 5.5%;
+  top: 40%;
   left: 63%;
+`;
+const FoxDonutImg = styled(LinkImg)`
+  position: absolute;
+  width: 5.5%;
+  top: 41.5%;
+  left: 73.5%;
+`;
+const CatDonutImg = styled(LinkImg)`
+  position: absolute;
+  width: 5.5%;
+  top: 42.5%;
+  left: 83.7%;
+`;
+const BirdDonutImg = styled(LinkImg)`
+  position: absolute;
+  width: 10%;
+  top: 60%;
+  left: 63%;
+}
+`;
+const HedgehogDonutImg = styled(LinkImg)`
+  position: absolute;
+  width: 6%;
+  top: 60%;
+  left: 76%;
+`;
+const FishDonutImg = styled(LinkImg)`
+  position: absolute;
+  width: 8.5%;
+  top: 60%;
+  left: 85.5%;
 `;
 const ModalImg = styled(LinkImg)`
   width: 100%;
